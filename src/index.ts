53946584fea0c55133ca8a3f584af3c785441b1b
Kevin Mok <mok.kevin11@gmail.com>
2023-04-19 21:29:25 -0500

    🚀 chore(eslint): add eslint configuration file 🚀 chore(gitignore): add build directory to gitignore 🚀 chore(package.json): update dependencies and scripts 🚀 feat(index.ts): add menu to select between asking a question or conversing with GPT 🚀 feat(utils): add terminal spinner to indicate when GPT is thinking 🚀 chore(tsconfig.json): add tsconfig.json file 🚀 chore(turbo.json): add turbo.json file The changes include adding eslint configuration file, adding build directory to gitignore, updating dependencies and scripts in package.json, adding a menu to select between asking a question or conversing with GPT, adding a terminal spinner to indicate when GPT is thinking, adding tsconfig.json file, and adding turbo.json file. These changes improve the code quality, add more flexibility, and improve the user experience.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,81 @@
-// src/index.ts
+import * as dotenv from 'dotenv';
 import * as readlineSync from 'readline-sync';
+import chalk from 'chalk';
 import { PromptTemplate } from 'langchain/prompts';
-import { LLMChain } from 'langchain/chains';
+import { LLMChain, ConversationChain } from 'langchain/chains';
 import { OpenAI } from 'langchain/llms/openai';
+import { BufferMemory } from 'langchain/memory';
+import { log, terminalSpinner } from './utils/index.js';
 
-const openai_api_key: string | undefined = process.env.OPENAI_KEY;
+dotenv.config();
 
-function promptUser(): string {
-  const input: string = readlineSync.question('Please enter some text: ');
+function promptUser(prompt: string): string {
+  const input: string = readlineSync.question(chalk.blue(prompt));
   return input;
 }
 
-async function main() {
-  const userInput: string = promptUser();
+async function askGPT(llm: OpenAI) {
+  const userInput: string = promptUser('What is your question: ');
   const template = `Question: {question}\nAnswer: `;
+
+  terminalSpinner.start();
+
   const prompt = new PromptTemplate({
-    template: template,
+    template,
     inputVariables: ['question'],
   });
-  const llm = new OpenAI({ temperature: 0.7, openAIApiKey: openai_api_key });
 
-  const chain = new LLMChain({ llm: llm, prompt: prompt });
-
+  const chain = new LLMChain({ llm, prompt });
   const response = await chain.run(userInput);
-  console.log(response);
+  terminalSpinner.stop();
+  log(chalk.green(response));
 }
+
+async function converseGPT(llm: OpenAI) {
+  const memory = new BufferMemory();
+  const chain = new ConversationChain({ llm, memory });
+
+  while (true) {
+    const input = promptUser('You: ');
+
+    if (input.toLowerCase() === 'exit') {
+      log(chalk.green('Goodbye!'));
+      break;
+    }
+
+    const { response } = await chain.call({ input });
+    log(chalk.green('AI: ', response));
+  }
+}
+
+async function main() {
+  const llm = new OpenAI({
+    modelName: 'gpt-3.5-turbo',
+    temperature: 0.7,
+    openAIApiKey: process.env.OPENAI_KEY,
+  });
+
+  log(
+    chalk.underline('Select an option:\n') +
+      chalk.bold.cyan('  1. Ask GPT a question\n') +
+      chalk.bold.greenBright('  2. Converse with GPT\n') +
+      chalk.bold.red('  3. Exit\n')
+  );
+  while (true) {
+    const userChoice = promptUser('Select an option: ');
+    switch (userChoice) {
+      case '1':
+        await askGPT(llm);
+        process.exit(0);
+      case '2':
+        await converseGPT(llm);
+        process.exit(0);
+      case '3':
+        process.exit(0);
+      default:
+        log(chalk.red('Invalid selection, please try again.'));
+    }
+  }
+}
+
 main();
---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+import ora from 'ora';
+export const { log } = console;
+
+export const terminalSpinner = ora({
+  text: 'Thinking',
+  color: 'green',
+  spinner: 'bounce',
+});
